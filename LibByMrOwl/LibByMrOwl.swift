29ee03079cf4d9fc52ca7188ea231559bf5f7d0b
IamWenboZhang <MrOwl@zhangwenbodeiMac.local>
2018-02-28 22:17:04 +0800

    扩展String和UIImage，新增算法类
    1.为String扩展了
     length属性（字符串长度）
    subscript方法 （直接使用Int数据做Index）
    split方法（分割字符串）
    
    2.为UIImage扩展了
    reSizeImage（重设图片大小）方法
    scaleImage（等比例缩放）方法
    
    3.新增算法类 提供了KMP算法

--- LibByMrOwl/LibByMrOwl.swift
+++ LibByMrOwl/LibByMrOwl.swift
@@ -38,5 +38,127 @@ class LibByMrOwl: NSObject {
         NSURLConnection.sendAsynchronousRequest(loadRequest, queue: loadQueue, completionHandler: completionHandler)
     }
     
-   
+    //根据Url获取网络图片
+    //参数：URL（图片的网络路径）
+    //返回值: 获得到的UIImage
+    static func getUIImageByUrl(URL: String) -> UIImage? {
+        var result:UIImage?
+        let data = NSData(contentsOfURL: NSURL(string: URL)!)
+        if(data != nil){
+            result = UIImage(data: data!)
+        }else{
+            result = nil
+        }
+        return result
+    }
+    
+    //本地缓存偏好设置
+    static func setDefault(key:String,value:AnyObject?){
+        if value == nil{
+            NSUserDefaults.standardUserDefaults().removeObjectForKey(key)
+        }else{
+            NSUserDefaults.standardUserDefaults().setObject(value, forKey: key)
+            NSUserDefaults.standardUserDefaults().synchronize() //同步
+        }
+    }
+    
+    //删除
+    static func removeUserDefault(key:String?){
+        if key != nil{
+            NSUserDefaults.standardUserDefaults().removeObjectForKey(key!)
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
+    }
+    
+    //获取设置
+    static func getDefault(key:String) ->AnyObject?{
+        return NSUserDefaults.standardUserDefaults().valueForKey(key)
+    }
+}
+
+
+
+class LibOfSuanfa:NSObject{
+    static func KMP(BigStr:String,SmallStr:String) -> Int{
+        var i = 0
+        var j = 0
+        var next:[Int]
+        LibOfSuanfa.GetNext(&next, Str: SmallStr)
+        while(i < BigStr.length && j < SmallStr.length){
+            if(j == -1 || BigStr[i] = SmallStr[j]){
+                i += 1
+                j += 1
+            }
+            else{
+                j = next[i]
+            }
+        }
+        if(j >= SmallStr.length){
+            return i - SmallStr.length
+        }
+        return -1
+    }
+    
+    private static func GetNext(inout next:[Int],Str:String){
+        var i = 0
+        var j = -1
+        next[0] = -1
+        while(j < Str.length - 1){
+            if(j == -1 || Str[i] == Str[j]){
+                i += 1
+                j += 1
+                if(Str[i] == Str[j]){
+                    next[i] = next[j]
+                }
+                else{
+                    next[i] = j
+                }
+            }
+            else{
+                j = next[j]
+            }
+        }
+    }
+}
+
+
+
+//扩展
+extension String{
+    
+    func split(splitStr: String) -> [String]{
+        let result = self.componentsSeparatedByString(splitStr)
+        return result
+    }
+    
+    var length:Int{
+        return self.characters.count
+    }
+    
+    subscript(index:Int) -> Character{
+        let charIndex = self.startIndex.advancedBy(index)
+        return self[charIndex]
+    }
+}
+
+extension UIImage {
+    /**
+     *  重设图片大小
+     */
+    func reSizeImage(reSize:CGSize)->UIImage {
+        //UIGraphicsBeginImageContext(reSize);
+        UIGraphicsBeginImageContextWithOptions(reSize,false,UIScreen.mainScreen().scale);
+        self.drawInRect(CGRectMake(0, 0, reSize.width, reSize.height));
+        let reSizeImage:UIImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return reSizeImage;
+    }
+    
+    /**
+     *  等比率缩放
+     */
+    func scaleImage(scaleSize:CGFloat)->UIImage {
+        let reSize = CGSizeMake(self.size.width * scaleSize, self.size.height * scaleSize)
+        return reSizeImage(reSize)
+    }
 }
